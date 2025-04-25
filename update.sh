52ee8b252a739dad797e56fd8d90a2b62e81f711
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2025-04-25 13:02:57 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/Ali-Murt4dho/vip/main/limit/menu.zip
+    wget https://github.com/hokagelegend9999/force/raw/refs/heads/main/limit/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
